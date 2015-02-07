a67241c508b62e3b2da65803c54cd78871f5ed76
Elisa Lescarret <lescarretelisa@yahoo.fr>
2015-02-07 23:46:26 +0100

    compatibilite

--- Projet_DB/src/correction/Compatibilite.java
+++ Projet_DB/src/correction/Compatibilite.java
@@ -1,25 +1,48 @@
 package correction;
 
-import java.util.Collection;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 
 public class Compatibilite {
 	//Compatibilite sur 90 points 
-	public static int verifierCorrespondance(Collection<Collection<Object>> resultatProfTrier, Collection<Collection<Object>> resultatEleveTrier){
+	public static int verifierCorrespondance(ArrayList<ArrayList<Object>> resultatProfTrier, ArrayList<ArrayList<Object>> resultatEleveTrier){
 		int score = 0;
 		int compatibilite = 0;
 		int cpt=0;
 		int cpt2 = 0;
+
 		while(cpt< resultatProfTrier.size() && cpt<resultatEleveTrier.size()){ 
 			cpt2 = 0;
-			Collection<Object> ligneResProf = resultatProfTrier.iterator().next();
-			Collection<Object> ligneResEleve = resultatEleveTrier.iterator().next();
-			while (cpt2< ligneResProf.size() && cpt2 < ligneResEleve.size())
+			
+			
+			while (cpt2< (resultatProfTrier.get(cpt)).size() && cpt2 < resultatEleveTrier.get(cpt).size())
 			{
-				Object elmtLigneProf = ligneResProf.iterator().next();
+				Object elmtProf = resultatProfTrier.get(cpt).get(cpt2);
+				Object elmtEleve = resultatEleveTrier.get(cpt).get(cpt2);
+	
+				
+				if(elmtProf == elmtEleve)
+				{
+					compatibilite ++;
+				}
 			}
 			
 			cpt++;
 		}
+		score= (compatibilite / resultatProfTrier.get(0).size()) * 90;
+		return score;
+	}
+	
+	public static int verifierNombreColonneNombreLigne(ResultSetMetaData rsmdProf, ResultSetMetaData rsmdEleve) throws SQLException
+	{
+		int score = 0;
+		if(rsmdProf.getColumnCount() ==  rsmdEleve.getColumnCount()){
+			
+		}
+		
+		
 		return score;
 	}
 }
