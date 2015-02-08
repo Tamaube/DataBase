a25db79742add786d663d5bb7a6abbc9fd429c47
Elisa Lescarret <lescarretelisa@yahoo.fr>
2015-02-08 02:19:02 +0100

    main

--- Projet_DB/src/main/CorrectionRequetes.java
+++ Projet_DB/src/main/CorrectionRequetes.java
@@ -1,10 +1,29 @@
 package main;
+import java.sql.ResultSet;
 import java.util.ArrayList;
 
+import correction.Compatibilite;
 import outils.*;
 
 public class CorrectionRequetes
 {
+	
+	public static int corrigerUneRequete(String requeteProf, String requeteEleve, ConnexionJDBC co)
+	{
+		int score = 0;
+		Compatibilite comp = new Compatibilite(co);
+		ResultSet rsProf = co.executerSelect(requeteProf);
+		ResultSet rsEleve = co.executerSelect(requeteEleve);
+		if(comp.verifierNombreColonneNombreLigne(rsProf, rsEleve) && 
+			comp.orderbyOK(requeteProf, requeteEleve))
+		{
+			ArrayList<ArrayList<String>> resultatEleveTrier = Tri.triRes(rsEleve);
+			ArrayList<ArrayList<String>> resultatProfTrier = Tri.triRes(rsProf);
+			score = comp.verifierCorrespondance(resultatProfTrier, resultatEleveTrier);
+		}
+		return score;
+		
+	}
 	public static void main(String args[])
 	{
 		if(args.length != 2)
@@ -14,9 +33,23 @@ public class CorrectionRequetes
 		}
 		
 		ConnexionJDBC co = new ConnexionJDBC();
-		
+		co.openConnection();
 		
 		ArrayList<String> ReqEleve = LecteurFichier.read(args[0]);
 		ArrayList<String> ReqCor = LecteurFichier.read(args[0]);
+		
+		int score = 0;
+		
+		int i=0; 
+		while(i<ReqCor.size() && i<ReqEleve.size()) 
+		{
+			score = score + CorrectionRequetes.corrigerUneRequete(ReqCor.get(i), ReqCor.get(i), co);
+			i++;
+		}
+		
+		score = score/ ReqCor.size();
+		System.out.println("Note: " + score + "/100");
+		
+		co.closeConnection();
 	}
 }
